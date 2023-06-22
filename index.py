db015a627f7715c2dc74032ee1cc22bcb05a2d56
April Joy Sia <amsia@up.edu.ph>
2023-06-22 16:00:14 +0800

    added admission reference

--- index.py
+++ index.py
@@ -12,6 +12,10 @@ def home():
 def register():
     return render_template('register.html')
 
+@app.route('/admissionreference')
+def adref():
+    return render_template('adrefscreen.html')
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
