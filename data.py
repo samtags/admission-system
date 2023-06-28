09067e0c91110770a01854a41aabb2359d6183a6
April Joy Sia <amsia@up.edu.ph>
2023-06-28 14:37:42 +0800

    added documentation

--- data.py
+++ data.py
@@ -3,16 +3,14 @@ from index import socketio
 
 db_path = 'records.db'
 
-# Connect to a database
-
-
+#Connect to a database
 def connect_db(path):
     conn = sqlite3.connect(path)
     # Convert tuples to dictionaries
     conn.row_factory = sqlite3.Row
     return (conn, conn.cursor())
 
-
+#Insert new registration into the database
 def register_student(payload):
     conn, cur = connect_db(db_path)
     query = 'INSERT INTO registrations (name, subject, email, phone, dob, status) VALUES (?,?,?,?,?,?)'
@@ -24,16 +22,15 @@ def register_student(payload):
               "pending"
               )
     cur.execute(query, values)
-
+    #Selects newly inserted row
     select = 'SELECT * FROM registrations WHERE id=?'
     values = (cur.lastrowid,)
     row = cur.execute(select, values).fetchone()
-
     conn.commit()
     conn.close()
     return row
 
-
+#Retreives data based on id
 def get_admission(id):
     conn, cur = connect_db(db_path)
     query = 'SELECT * FROM registrations WHERE id=?'
@@ -42,7 +39,7 @@ def get_admission(id):
     conn.close()
     return row
 
-
+#Removes registration from database
 def remove_admission(id):
     conn, cur = connect_db(db_path)
     query = 'DELETE FROM registrations WHERE id=?'
@@ -52,7 +49,7 @@ def remove_admission(id):
     conn.close()
     return True
 
-
+#Selects all registrations from the database
 def get_all_admissions():
     conn, cur = connect_db(db_path)
     query = 'SELECT * FROM registrations'
@@ -60,7 +57,7 @@ def get_all_admissions():
     conn.close()
     return row
 
-
+#Retrieves all registrations that have a specific status
 def get_admissions_by_status(status):
     conn, cur = connect_db(db_path)
     query = 'SELECT * FROM registrations WHERE status=?'
@@ -69,7 +66,7 @@ def get_admissions_by_status(status):
     conn.close()
     return row
 
-
+#Updates the status of a registration
 def update_admission_status(id, status):
     conn, cur = connect_db(db_path)
     query = 'UPDATE registrations SET status=? WHERE id=?'
@@ -79,7 +76,7 @@ def update_admission_status(id, status):
     conn.close()
     return True
 
-
+#Retrieves registrations that have a specific name, subject, or status
 def get_admissions():
     conn, cur = connect_db(db_path)
     cur.execute('SELECT name, subject, status FROM registrations')
@@ -87,7 +84,7 @@ def get_admissions():
     conn.close()
     return admissions
 
-
+#Searches the database for pending registrations with a specific name and subject and returns all registrations that match
 def search_by_name_and_subject(name, subject):
     conn, cur = connect_db(db_path)
     query = "SELECT * FROM registrations WHERE name = ? AND subject = ? AND status='pending'"
@@ -97,7 +94,7 @@ def search_by_name_and_subject(name, subject):
     conn.close()
     return results
 
-
+#searches the database with a specific name and subject, and returns all registrations that match
 def mastersearch_by_name_and_subject(name, subject):
     conn, cur = connect_db(db_path)
     query = "SELECT * FROM registrations WHERE name = ? AND subject = ?"
